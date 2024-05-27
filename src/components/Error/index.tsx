207bc9215881afeaf1fc6a67c185b53ad278f628
Fábio <fabiogoncalvesk2001@gmail.com>
2024-05-27 11:41:46 +0100

    deployment changes

--- src/components/Error/index.tsx
+++ src/components/Error/index.tsx
@@ -7,7 +7,7 @@ export const Error = () => {
       <Div>
         <MessageContainer>
           <h1>Oops!</h1>
-          <p>You're in the wrong place.</p>
+          <p>You're in the wrong place.</p> 
           <p>DO NOT DISTURB THE CAT PLEASE!</p>
         </MessageContainer>
         <CatContainer>
